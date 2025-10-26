85074efda8a62c76bd44652a2418af481856b08c
Petr Losev <p.losev@leningrad.io>
2025-10-26 21:09:55 +0300

    update project, css, ts Squashed commit of the following:
    commit 1e4f65cfac982a2d9e009607d7d96cdd59727d9d
    Author: Petr Losev <p.losev@leningrad.io>
    Date:   Sun Oct 26 21:05:48 2025 +0300
    
        add TS on project
    
    commit dfc1ba8e75ad5d9292bf4306264a478f4a2f4b84
    Author: Petr Losev <p.losev@leningrad.io>
    Date:   Sun Oct 26 18:32:42 2025 +0300
    
        add css vars
    
    commit 3e5779094da2a252213db2fbb1d5663d35ca0ec4
    Author: Petr Losev <p.losev@leningrad.io>
    Date:   Sun Oct 26 17:23:19 2025 +0300
    
        delete folder inside components

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
